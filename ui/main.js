a8c9a492a2a6aa7799207e259309ef45d98a7839
saikrishnaps <saikrshn27@gmail.com>
2016-10-27 16:38:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('madi');
 var marginLeft=0;
-function moveright(){
+function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick= function(){
-    var interval=setInterval(moveright,100);
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+    
 };
