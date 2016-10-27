080f734f8e8801225043212908b8f73fb386c8fd
saikrishnaps <saikrshn27@gmail.com>
2016-10-27 16:32:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveright(){
     marginLeft=marginLeft+10;
-    img.style.marginLeft='10px';
+    img.style.marginLeft=margiLeft+'px';
 }
 img.onclick= function(){
     var interval=setInterval(moveright,100);
