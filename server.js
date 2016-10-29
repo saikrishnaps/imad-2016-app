19452b67979b39d7e371edd12598e7cbf60c07de
saikrishnaps <saikrshn27@gmail.com>
2016-10-29 18:03:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/Mario_(New_Super_Mario_Bros._2).png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
