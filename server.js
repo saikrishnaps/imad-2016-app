ac3291332e620f8d7065885739236ed7cc1ccd2d
saikrishnaps <saikrshn27@gmail.com>
2016-10-29 12:45:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send(crateTemplate(articleOne));
+  res.send(createTemplate(articleOne));
   
 });
 app.get('/article-two', function (req, res) {
