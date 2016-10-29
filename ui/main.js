67c9e3fbcd33d96a05da4ac570e9813b6b83e386
saikrishnaps <saikrshn27@gmail.com>
2016-10-30 00:40:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,8 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='new value has been deployed';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick= function(){
-    var interval=setInterval(moveRight,100);
+var button=document.getElemetById('counter');
+var counter=0;
+button.onclick= function() {
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
     
 };
