aa9d501e616b11ee6b3f290ce8f312c682c2f4ff
saikrishnaps <saikrshn27@gmail.com>
2016-10-30 18:06:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,25 +1,5 @@
- var express = require('express');
- var morgan = require('morgan');
- var path = require('path');
-  var app = express();
- app.use(morgan('combined'));
- app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
- });
 
-app.get('/ui/style.css', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
- });
- 
- app.get('/ui/madi.png', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
- });
 
- var port = 8080;
- app.listen(8080, function () {
-   console.log(`IMAD course app listening on port ${port}!`);
- });
- /* 
  var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -124,5 +104,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 }); 
-
- */
