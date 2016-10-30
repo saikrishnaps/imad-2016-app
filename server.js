8b6ee103d4f32ae85c34256b4cb0ee0f722f2259
saikrishnaps <saikrshn27@gmail.com>
2016-10-30 13:06:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,3 +19,110 @@ app.get('/ui/style.css', function (req, res) {
  app.listen(8080, function () {
    console.log(`IMAD course app listening on port ${port}!`);
  });
+ /* 
+ var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+    var articles={
+    'article-one':{
+         title1:'article one',
+        heading:'Article-one',
+        date:'sep 5, 2016',
+        content:`<p>
+               this is the content containing the article one .this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one.
+           </p>
+           <p>
+               this is the content containing the article one .this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one 
+           </p><p>
+               this is the content containing the article one .this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one this is the content containing the article one 
+           </p>`
+},
+    'article-two':{
+         title:'article Two',
+        heading:'Article-two',
+        date:'sep 23, 2016',
+        content:`<p>
+               this is the content containing the article Two.
+           </p>`
+},
+    'article-three':{
+         title:'article Three',
+        heading:'Article-three',
+        date:'sep 5, 2016',
+        content:`<p>
+               this is the content containing the article Two.
+           </p>`
+}
+};
+
+function createTemplate(data){
+    var titles=data.title1;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmlTemplate=`
+<html>
+    <head>
+        <title>
+            ${titles}
+        </title>
+    <meta name="viewport"  content="width-device-width,initial-scale-1" /> 
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+       <div>
+           <a href="/">Home</a>
+       </div> 
+       <hr/>
+       <h3>
+           ${heading}
+       </h3>
+       <div>
+           ${date}
+       </div>
+       <div>
+           ${content}
+       </div>
+       </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
+});
+app.get('/:articleName', function (req, res) {
+  var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+}); 
+
+ */
