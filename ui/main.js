a99e0c005313f0952b40c1a8f8859ac613893a00
saikrishnaps <saikrshn27@gmail.com>
2016-10-30 23:15:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,1 @@
-/*var button=document.getElementById('counter');
-var counter=0;
-button.onclick= function() {
-    var request=new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if(request.readystate===XMLHttpRequest.DONE){
-          if(request.status===200){
-              var counter=request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-      }  
-    };
-    request.open('GET','http://saikrishnaps.imad.hasura-app.io',true);
-    request.send(null);
-};*/
+console.log('loaded');
