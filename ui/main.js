134028fa9da65b76daeb223e5464cffc85950b31
saikrishnaps <saikrshn27@gmail.com>
2016-10-31 13:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -74,7 +74,7 @@ if(request.status==200){
 document.getElementById('counterTxt').innerHTML=this.responseText;
 }
 };
-request.open('GET',"http://saikrishnaps.imad.hasura-app.io/counter",true);
+request.open('GET',"http://saikrishnaps.imad.hasura-app.io/sai/counter",true);
 request.send();
 };
 
