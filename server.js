a9bff623029b15801740feb29a70c092dd5d89f5
saikrishnaps <saikrshn27@gmail.com>
2016-11-12 21:53:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var counter=0;
-app.get('/sai/counter',function(req, res){
+app.get('/counter',function(req, res){
 counter = counter + 1;
 res.send(counter.toString());
 });
@@ -121,7 +121,7 @@ res.send(JSON.stringify(names));
 
 
 
-app.get('/sai/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
  var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
  });
