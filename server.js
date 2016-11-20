4d1e315af148fc2dd00261137c750ace5d7442e9
saikrishnaps <saikrshn27@gmail.com>
2016-11-20 12:57:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,9 +100,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-
-
-
 var counter=0;
 app.get('/counter',function(req, res){
 counter = counter + 1;
